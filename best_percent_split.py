4c848a0ce05fbe14e1ef59144b5db02596f18d0d
ltb33 <ltb33@pitt.edu>
2026-02-06 21:53:24 -0500

    added_class_data

---
+++ best_percent_split.py
@@ -0,0 +1,28 @@
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import LabelEncoder
+from torch.utils.data import Dataset, DataLoader
+
+class Best_Percent_Split(Dataset):
+    def __init__(self, data):
+        self.texts = data['text'].values
+        self.labels = data['label'].values
+    
+    def __len__(self):
+        return len(self.texts)
+    
+    def __getitem__(self, idx):
+        text = self.texts[idx]
+        label = self.labels[idx]
+        return torch.tensor(text, dtype=torch.long), torch.tensor(label, dtype=torch.long)
+
+train_dataset = SentimentDataset(train_data)
+test_dataset = SentimentDataset(test_data)
+
+train_loader = DataLoader(train_dataset, batch_size=32, shuffle=True)
+test_loader = DataLoader(test_dataset, batch_size=32, shuffle=False)
